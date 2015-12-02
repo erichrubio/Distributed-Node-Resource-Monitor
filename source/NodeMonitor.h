dba813f1613f22fcf9c966ccb92ef3dc591dbb3c
Zachary <zachary.m.barton@vanderbilt.edu>
2015-12-02 01:23:00 +0000

    Adding NodeMonitor class

---
+++ source/NodeMonitor.h
@@ -0,0 +1,26 @@
+//Names: Zachary Barton
+//       Eric Rubio
+//Course: CS3281
+//Section: Fall 2015
+
+//This class gathers information from the Node where it is deployed,
+//then sends that information back to the central node via LCM.
+
+#include <string>
+#include <sys/vfs.h> //Used to obtain file system usage info
+
+
+
+class NodeMonitor{
+    private:
+        //LCM target URL
+        std::string lcm_url;
+        //
+
+    public:
+        getCPUusage();
+        getMemUsage();
+        getStorageUsage();
+        getNetworkUsage();
+
+};
